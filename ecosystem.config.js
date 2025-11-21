85860c549599568d6478d9ebb2d8352fb563ad33
Samyak Chaudhary <alvysamy@gmail.com>
2025-11-21 20:25:06 +0545

    fix: Correct next binary path to node_modules/.bin/next

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -37,7 +37,7 @@ module.exports = {
     {
       name: 'ai-shorts-frontend',
       cwd: './frontend',
-      script: 'node_modules/next/dist/bin/next',
+      script: './node_modules/.bin/next',
       args: 'start -p 3000',
       instances: 1,
       exec_mode: 'fork',
